825080afca9208bf1293f7802d850d39057faf02
missgevan <leviagevansekyi@gmail.com>
2022-11-11 10:13:56 -0800

    Added handlers

--- shell.h
+++ shell.h
@@ -13,5 +13,15 @@ int _strcmp(char *str, char *str1);
 int _strncmp(char *str, char *str1, size_t n);
 char _strdup(char *str);
 
+/*exec_commands*/
+char **tokenizer(char *str);
+void exec_cmd(char *c, char **cmd);
+void free_command(char **cmd);
+
+/*builtin*/
+int builtin_checker(char **cmd, char *b);
+void logout(char **cmd, char *b);
+void env_builtin(void);
+
 
 #endif /* SHELL_H */
